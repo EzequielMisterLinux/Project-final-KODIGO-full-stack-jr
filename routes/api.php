bf2c28e04e0eb59eda755d210d73b8c74c1270cb
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2025-01-26 16:35:25 -0600

    fixed routes for get json and pages react router dom

--- routes/api.php
+++ routes/api.php
@@ -5,10 +5,9 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\UserController;
 
-// Rutas públicas para el registro y login
+
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
-
 
 Route::middleware('auth:api')->group(function () {
     Route::get('users', [UserController::class, 'index']);
@@ -16,13 +15,14 @@ Route::middleware('auth:api')->group(function () {
     Route::post('users', [UserController::class, 'store']);
     Route::put('users/{id}', [UserController::class, 'update']);
     Route::delete('users/{id}', [UserController::class, 'destroy']);
+    
     Route::get('me', [AuthController::class, 'me']);
     Route::post('logout', [AuthController::class, 'logout']);
-
+    
     Route::get('products', [ProductController::class, 'index']);
     Route::get('products/{id}', [ProductController::class, 'show']);
     Route::post('products', [ProductController::class, 'store']);
     Route::put('products/{id}', [ProductController::class, 'update']);
     Route::delete('products/{id}', [ProductController::class, 'destroy']);
-    
 });
+
