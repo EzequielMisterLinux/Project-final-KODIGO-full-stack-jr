d6c3ebafabed9a1ae8e3e475b5b3b2f0067f1873
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2025-01-31 13:32:06 -0600

    Fix HTTPS config for Railway

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/{path?}', function () {
-    return view('welcome');
-})->where('path', '^(?!api).*');
-
+    return view('welcome', [
+        'assets' => mix('resources/js/main.jsx') // Usar mix() para assets
+    ]);
+})->where('path', '^(?!api|docs|sanctum).*$');
