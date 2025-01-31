e4a1188275cea7082c2ac3dea0813bcabd5ab700
EzequielMisterLinux <humbertoezequiel.z.c@gmail.com>
2025-01-31 13:37:42 -0600

    Fix HTTPS config for Railway

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/{path?}', function () {
-    return view('welcome', [
-        'assets' => mix('resources/js/main.jsx') // Usar mix() para assets
-    ]);
-})->where('path', '^(?!api|docs|sanctum).*$');
+    return view('welcome');
+})->where('path', '^(?!api).*');
+
--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,7 @@ import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-
-  base: process.env.NODE_ENV === 'production' 
-  ? 'https://project-final-kodigo-full-stack-production.up.railway.app/' 
-  : '/',
-build: {
-  manifest: true, // Generar manifest para cache
-},
-
+  
   plugins: [
     laravel({
       input: [
